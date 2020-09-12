e76b045b773a40dfb07ab28975df88df588d519e
oliveirafab16 <fabriciocarvalho932@gmail.com>
2020-09-12 00:03:37 -0300

    arte

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _AsciiArt_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("||  || ");
+            Console.WriteLine("\\ () //" );
+            Console.WriteLine("//(__)\\");
+             Console.WriteLine("||  ||");
+            
+            
+
+
+        }
+    }
+}
